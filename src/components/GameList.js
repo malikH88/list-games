3517edbdb2b035b3a7d12c1be1b65824d14fc7d9
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-05-07 15:32:59 +0200

    feat(gamelist): add component

---
+++ src/components/GameList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class GameList extends React.Component {
+  render() {
+    return (
+      <p>hello world from GameList</p>
+    )
+  }
+}
+
+export default GameList;
